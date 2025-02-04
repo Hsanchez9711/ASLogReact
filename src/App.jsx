54245d7f32e3fc4fa9987b3baf61d1568cb8c600
Hector Sanchez <hectorsanchez97@yahoo.com>
2025-02-04 16:18:59 -0700

    feat: updates not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import Body from "./components/body";
-// import "./App.css";
-
-// TODO here make a modularized info box that we can reuse throughout the site
 
 function App() {
   return (
